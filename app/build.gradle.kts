5ca2c51f66f98d962f70f274c306e603a6c37a57
thiagotfer <thiagotfer@icloud.com>
2025-10-30 23:00:09 -0300

    Start commit!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,12 +43,16 @@ dependencies {
         implementation("androidx.activity:activity-compose")
         implementation("androidx.lifecycle:lifecycle-runtime-compose")
 
-        // ✅ CORREÇÃO APLICADA AQUI
+
         implementation("androidx.navigation:navigation-compose:2.7.7")
 
         implementation("androidx.compose.material:material-icons-extended")
         implementation("androidx.compose.material3:material3-window-size-class")
         debugImplementation("androidx.compose.ui:ui-tooling")
         debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     }
 }
